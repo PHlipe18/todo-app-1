a7a302a09300482af665d291664b2744650e8883
Philipe da Silva <philippsilva018@gmail.com>
2023-11-16 12:39:12 -0300

    adição dos ícones de lua e de sol para troca de tema

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express()
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
 
+app.set(express.static('public'))
+
 app.get ('/', (requisicao, resposta) => {
     resposta.render('home')
 })
